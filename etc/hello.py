6dc5a7fee764b278d0934c83145e6e26eb08a6d9
JaeHyun <nam2350@gmail.com>
2020-05-28 19:48:22 +0900

    파이썬 환경설정

--- etc/hello.py
+++ etc/hello.py
@@ -1,10 +1,29 @@
-import requests 
+# -*-coding: utf-8-*-
+import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-rjson = r.json()
+# URL을 읽어서 HTML를 받아오고,
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
+}
+data = requests.get(
+    "https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303",
+    headers=headers,
+)
 
-gus = rjson['RealtimeCityAir']['row']
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, "html.parser")
 
-for gu in gus:
-	if gu['IDEX_MVL'] < 53:
-		print (gu['MSRSTE_NM'], gu['IDEX_MVL'])
+# select를 이용해서, tr들을 불러오기
+movies = soup.select("#old_content > table > tbody > tr")
+
+# movies (tr들) 의 반복문을 돌리기
+
+for movie in movies:
+    # movie 안에 a 가 있으면,
+    a_tag = movie.select_one("td.title > div > a")
+    if a_tag is not None:
+        rank = movie.select_one("td:nth-child(1) > img")["alt"]  # img 태그의 alt 속성값을 가져오기
+        title = a_tag.text  # a 태그 사이의 텍스트를 가져오기
+        star = movie.select_one("td.point").text  # td 태그 사이의 텍스트를 가져오기
+        print(rank, title, star)
