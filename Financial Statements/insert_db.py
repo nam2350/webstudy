8b18fc8ec4588a72c253aa30173074d95bfffcf9
JaeHyun <nam2350@gmail.com>
2020-06-29 15:19:58 +0900

    db 회사정보 회사코드 insert

--- Financial Statements/insert_db.py
+++ Financial Statements/insert_db.py
@@ -8,7 +8,14 @@ db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
 with open('dart_name_code.json') as json_file:
     json_data = json.load(json_file)
 
-    corp_name = json_data["result"]["list"]
+    corp_data = json_data["result"]["list"]
     # print(corp_name)
     
-    
+    for corp_codes_names in corp_data:
+        corp_name = corp_codes_names["corp_name"]
+        corp_code = corp_codes_names["corp_code"]
+        modify_date = corp_codes_names["modify_date"]
+
+        doc = {"name" : corp_name, "code" : corp_code, "modify_date" : modify_date}
+        db.corp_data.insert_one(doc)
+        
